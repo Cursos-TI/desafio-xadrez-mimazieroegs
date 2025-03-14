7a844307ad77dc42bab50de55f3d494841753089
Michelle Maziero <micomprasmaziero@gmail.com>
2025-03-14 22:10:31 +0000

    Criação das regras

--- xadrez.c
+++ xadrez.c
@@ -2,12 +2,30 @@
 
 // Desafio de Xadrez - MateCheck
 int main() {
-    
+    int i;
+
     printf("*** DESAFIO DE XADREZ ***\n");
 
-    
-
-
-
-    return 0;
+ //Movimento da Torre - 5 casas para a direita
+ printf("\n**Movimento da Torre**\n");
+ for (i = 1; i <= 5; i++) {
+    printf("Direita\n");
+ }
+//Movimento da Torre - 5 casas na diagonal - cima e direita
+printf("\n**Movimento do Bispo**\n");
+i = 1;
+while (i <= 5){
+    printf("Cima, Direita\n");
+    i++;
+}
+//Movimento da Rainha - 8 casas para a esquerda
+ printf("\n**Movimento da Rainha**\n");
+ i = 1;
+ do
+ {
+    printf("Esquerda\n");
+    i++;
+ } while (i <= 8);
+ 
+     return 0;
 }
